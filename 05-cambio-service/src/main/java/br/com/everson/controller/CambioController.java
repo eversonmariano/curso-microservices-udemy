b370440e92d1b5aeb626c479281169ebaa7ac9a2
Everson Mariano <mariano.computacao@gmail.com>
2022-04-23 22:10:08 -0300

    Cambio Service testado com duas portas

--- 05-cambio-service/src/main/java/br/com/everson/controller/CambioController.java
+++ 05-cambio-service/src/main/java/br/com/everson/controller/CambioController.java
@@ -1,2 +1,32 @@
-package br.com.everson.controller;public class CambioController {
+package br.com.everson.controller;
+
+import br.com.everson.model.Cambio;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.math.BigDecimal;
+
+@RestController
+@RequestMapping("cambio-service")
+public class CambioController {
+
+    @Autowired
+    private Environment environment;
+
+    @GetMapping(value="{amount}/{from}/{to}")
+    public Cambio getCambio(
+            @PathVariable("amount") BigDecimal amount,
+            @PathVariable("from") String from,
+            @PathVariable("to") String to
+    ){
+        var port = environment.getProperty("local.server.port");
+
+        return new Cambio(
+                1L, from, to, BigDecimal.ONE, BigDecimal.ONE, port);
+    }
+
 }
