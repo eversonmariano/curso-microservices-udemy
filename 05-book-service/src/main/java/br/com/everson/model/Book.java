238ac8c9a25de41939c1b9b742990be0eb836e51
Everson Mariano <mariano.computacao@gmail.com>
2022-04-24 17:31:53 -0300

    Config. JPA no .yml + anotações do JPA + integração + migrations

---
+++ 05-book-service/src/main/java/br/com/everson/model/Book.java
@@ -0,0 +1,64 @@
+package br.com.everson.model;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.Objects;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.persistence.Transient;
+
+@Entity(name = "book")
+public class Book  implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "author", nullable = false, length = 180)
+    private String author;
+    @Column(name = "launch_date", nullable = false)
+    @Temporal(TemporalType.DATE)
+    private Date launchDate;
+    @Column(nullable = false)
+    private Double price;
+    @Column(nullable = false, length = 250)
+    private String title;
+    @Transient
+    private String currency;
+    @Transient
+    private String environment;
+
+    public Book() {
+    }
+
+    public Book(Long id, String author, Date launchDate, Double price, String title, String currency, String environment) {
+        this.id = id;
+        this.author = author;
+        this.launchDate = launchDate;
+        this.price = price;
+        this.title = title;
+        this.currency = currency;
+        this.environment = environment;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return Objects.equals(id, book.id) && Objects.equals(author, book.author) && Objects.equals(launchDate, book.launchDate) && Objects.equals(price, book.price) && Objects.equals(title, book.title) && Objects.equals(currency, book.currency) && Objects.equals(environment, book.environment);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, author, launchDate, price, title, currency, environment);
+    }
+}
