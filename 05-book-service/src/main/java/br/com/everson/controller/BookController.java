f856878f40cd3d06c57af798af206889dd7b5013
Everson Mariano <mariano.computacao@gmail.com>
2022-04-24 18:53:37 -0300

    Integrando o Book Service ao BD com JPA e Spring Data + migrations+ Flyway

---
+++ 05-book-service/src/main/java/br/com/everson/controller/BookController.java
@@ -0,0 +1,42 @@
+package br.com.everson.controller;
+
+import br.com.everson.model.Book;
+
+import br.com.everson.repository.BookRepository;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+
+
+@RestController
+@RequestMapping("book-service")
+public class BookController {
+
+    @Autowired
+    private Environment environment;
+
+
+    @Autowired
+    private BookRepository repository;
+
+
+    @GetMapping(value = "/{id}/{currency}")
+    public Book findBook(@PathVariable("id") Long id, @PathVariable("currency") String currency) {
+
+        var book = repository.getById(id);
+        if (book == null) throw new RuntimeException("Book not found");
+
+        var port = environment.getProperty("local.server.port");
+        return book;
+
+
+    }
+
+}
